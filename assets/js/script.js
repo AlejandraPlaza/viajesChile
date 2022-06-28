7596423ef0371c0d430b3bf64eb8879f3862829b
Alejandra Plaza <alejandra.plaza.herrera@gmail.com>
2022-06-28 17:16:31 -0400

    modifica sección íconos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
+//Tooltip Icon-section
 var tooltipTriggerList =
 [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+//Toggle cards
+$(document).ready(function () {
+    $(".card-title").click(function () {
+        $(".card-text").toggle();
+    });
+});
