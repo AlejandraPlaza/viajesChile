5d9a789b871441ac12c24c7e2cdf08a162474920
Alejandra Plaza <alejandra.plaza.herrera@gmail.com>
2022-06-28 22:05:12 -0400

    agrega smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 //Tooltip Icon-section
 var tooltipTriggerList =
-[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-return new bootstrap.Tooltip(tooltipTriggerEl)
+    return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
 //Toggle cards
@@ -11,3 +11,16 @@ $(document).ready(function () {
         $(".card-text").toggle();
     });
 });
+
+//SmoothScroll
+$(document).ready(function () {
+    $("a").click(function () {
+        var gato = this.hash
+        $("html, body").animate(
+            {
+                scrollTop: $(gato).offset().top
+            },
+            800
+        )
+    })
+})
